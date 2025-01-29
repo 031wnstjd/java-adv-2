5065e219d2a27e2b1ff5370cdf15f06b4e830a69
Junsung <031wnstjd@naver.com>
2025-01-29 19:02:08 +0900

    자바 직렬화 관련 주석 내용 추가

--- src/io/member/Member.java
+++ src/io/member/Member.java
@@ -2,7 +2,16 @@ package io.member;
 
 import java.io.Serializable;
 
-// Serializable(마커 인터페이스)이 없으면 객체 직렬화 시 NoSerializableException이 발생함
+/**
+ * - Serializable(마커 인터페이스)이 없으면 객체 직렬화 시 NoSerializableException이 발생함
+ * - 현대에는 객체 직렬화를 거의 사용하지 않음 (더 좋은 대안 기술들이 많음)
+ *   [객체 직렬화를 사용하지 않는 이유]
+ *   - 버전 관리의 어려움 (클래스 구조가 변경되면 이전에 직렬화된 객체와의 호환성 문제가 발생함)
+ *   - 플랫폼 종속성 (자바 직렬화는 자바 플랫폼에 종속적이어서 다른 언어나 시스템과의 상호 운용성이 떨어짐)
+ *   - 성능 이슈 (직렬화/역직렬화 과정이 상대적으로 느리고, 리소스를 많이 사용한다.)
+ *   - 유연성 부족 (직렬화된 형식을 커스터마이즈하기 어렵다.)
+ *   - 크기 효율성 (직렬화된 데이터 크기가 상대적으로 크다.)
+ */
 public class Member implements Serializable {
     private String id;
     private String name;
