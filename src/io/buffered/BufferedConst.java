ceeb440f7618866a6e62dfb0fd00b37007090187
Junsung <031wnstjd@naver.com>
2025-01-29 14:22:00 +0900

    파일 입출력과 성능 최적화2 -버퍼 활용

--- src/io/buffered/BufferedConst.java
+++ src/io/buffered/BufferedConst.java
@@ -3,5 +3,5 @@ package io.buffered;
 public class BufferedConst {
     public static final String FILE_NAME = "temp/buffered.dat";
     public static final int FILE_SIZE = 10 * 1024 * 1024; // 10MB
-    public static final int BUFFER_SIZE = 8192; // 8KB
+    public static final int BUFFER_SIZE = 8192; // 8KB (디스크나 파일 시스템에서 읽고 쓰는 기본 단위는 보통 4KB, 8KB)
 }
