f169bbfb8f38228197dd1bec0efd1e37fe5cb567
Junsung <031wnstjd@naver.com>
2025-02-09 22:43:52 +0900

    리플렉션 - 활용 예제(공통 Util)

---
+++ src/reflection/FieldUtil.java
@@ -0,0 +1,23 @@
+package reflection;
+
+import java.lang.reflect.Field;
+
+public class FieldUtil {
+
+    public static void nullFieldToDefault(Object target) throws IllegalAccessException {
+        Class<?> clazz = target.getClass();
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            if (field.get(target) != null) {
+                continue;
+            }
+
+            if (field.getType() == String.class) {
+                field.set(target, "");
+            } else if (field.getType() == Integer.class) {
+                field.set(target, 0);
+            }
+        }
+    }
+}
